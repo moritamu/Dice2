ff202ee6299f8288f5497aa27827841b8d647eb0
morisum <moris@r.sannet.ne.jp>
2023-04-08 17:26:10 +0900

    Initial Commit

---
+++ Dice2/Dice2App.swift
@@ -0,0 +1,17 @@
+//
+//  Dice2App.swift
+//  Dice2
+//
+//  Created by MsMacM on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct Dice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
